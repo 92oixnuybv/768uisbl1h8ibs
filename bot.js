c9d15494b9a7617e5e6914969f4647dfd6b6e3fa
92oixnuybv <43111344+92oixnuybv@users.noreply.github.com>
2018-09-09 15:32:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,14 @@
-client.on('ready', () => {
+
+const ytdl = require("ytdl-core");
+const { Client, Util } = require('discord.js');
+const getYoutubeID = require('get-youtube-id');
+const fetchVideoInfo = require('youtube-info');
+const YouTube = require('simple-youtube-api');
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+const queue = new Map();
+const client = new Discord.Client();
+
+client.on('ready', () => {	
     console.log(`Logged in as ${client.user.tag}!`);
     console.log(`in ${client.guilds.size} servers `)
     console.log(`[xd] ${client.users.size}`)
@@ -371,3 +381,4 @@ collector7.on('collect', r => {
 })
 }
 });
+client.login(process.env.BOT_TOKEN);
